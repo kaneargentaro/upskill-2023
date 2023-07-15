1febe3177e136d4b9d64685237bbe9fc12da5b40
Kane Argentaro <k.argentaro@optergy.com>
2023-07-15 19:18:42 +1000

    feat: updated multiplePointers countUniqueValues.js

--- JavaScript Algorithms and Data Structures Masterclass/05. Problem Solving Patterns/32. Count Unique Values Challenge/countUniqueValues.js
+++ JavaScript Algorithms and Data Structures Masterclass/05. Problem Solving Patterns/32. Count Unique Values Challenge/countUniqueValues.js
@@ -54,9 +54,10 @@ function countUniqueValues(arr) {
  * then return the length that we figure out from i
  */
 function countUniqueValues1(arr) {
-    if (arr.length === 0) return 0
+    if (arr.length === 0) 
+        return 0
     let i = 0;
-    for (let j = 1; j < arr.length; j++ )
+    for (let j = 1; j < arr.length; j++ ) {
         if (arr[i] !== arr[j]) {
             i++
             arr[i] = arr[j]; 
