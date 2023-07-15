2cecb0cd86cba698dfcef592800da0fdffd092b0
Kane Argentaro <k.argentaro@optergy.com>
2023-07-15 19:18:17 +1000

    feat: multiple pointers teacher example

--- JavaScript Algorithms and Data Structures Masterclass/05. Problem Solving Patterns/32. Count Unique Values Challenge/countUniqueValues.js
+++ JavaScript Algorithms and Data Structures Masterclass/05. Problem Solving Patterns/32. Count Unique Values Challenge/countUniqueValues.js
@@ -45,7 +45,34 @@ function countUniqueValues(arr) {
     return counter;
 }
 
-if (countUniqueValues([1, 1, 1, 1, 1, 2]) === 2) console.log("test 1 passed");
-if (countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) === 7) console.log("test 2 passed");
-if (countUniqueValues([]) === 0) console.log("test 3 passed");
-if (countUniqueValues([-2, -1, -1, 0, 1]) === 4) console.log("test 4 passed");
+/**
+ * Teachers example
+ * 
+ * Rebuild the array like this
+ * [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13] => [1, 2, 3, 4, 7, 12, 13, x, x, x, x]
+ * 
+ * then return the length that we figure out from i
+ */
+function countUniqueValues1(arr) {
+    if (arr.length === 0) return 0
+    let i = 0;
+    for (let j = 1; j < arr.length; j++ )
+        if (arr[i] !== arr[j]) {
+            i++
+            arr[i] = arr[j]; 
+        } 
+    }
+    return i + 1;
+}
+
+if (countUniqueValues([1, 1, 1, 1, 1, 2]) === 2) 
+    console.log("test 1 passed");
+
+if (countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) === 7) 
+    console.log("test 2 passed");
+
+if (countUniqueValues([]) === 0) 
+    console.log("test 3 passed");
+
+if (countUniqueValues([-2, -1, -1, 0, 1]) === 4) 
+    console.log("test 4 passed");
