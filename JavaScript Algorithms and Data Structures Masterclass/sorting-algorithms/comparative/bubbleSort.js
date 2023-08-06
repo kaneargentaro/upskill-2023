3eca3143ef1f6150bd9ec2062e792a56985c75f3
Kane Argentaro <k.argentaro@optergy.com>
2023-08-06 13:51:42 +1000

    feat: exercise 44 bubbleSort.js

--- JavaScript Algorithms and Data Structures Masterclass/sorting-algorithms/comparative/bubbleSort.js
+++ JavaScript Algorithms and Data Structures Masterclass/sorting-algorithms/comparative/bubbleSort.js
@@ -1,39 +1,39 @@
 /**
- * 
- * @param {Number[]} arr 
+ *
+ * @param {Number[]} arr
  */
 function bubbleSort(arr) {
-    let noSwaps = false;
+	let noSwaps = false;
 
-    // Work from the end and work our way down because the 
-    // last element is sorted and we don't need to resort it each loop
-    for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            noSwaps = true;
-            if (arr[j] > arr[j+1]) {
-                swap(arr, j, j + 1);
-                noSwaps = false;
-            }
-        }
+	// Work from the end and work our way down because the
+	// last element is sorted and we don't need to resort it each loop
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; j++) {
+			noSwaps = true;
+			if (arr[j] > arr[j + 1]) {
+				swap(arr, j, j + 1);
+				noSwaps = false;
+			}
+		}
 
-        // If no values are swapped, then break out
-        // Arr has been sorted. If we don't break, it'll keep
-        // executing unnecessarily. 
-        if (noSwaps) break;
-    }
-    return arr;
+		// If no values are swapped, then break out
+		// Arr has been sorted. If we don't break, it'll keep
+		// executing unnecessarily.
+		if (noSwaps) break;
+	}
+	return arr;
 }
 
 /**
- * 
- * @param {Number[]} arr 
- * @param {Number} index1 
- * @param {Number} index2 
+ *
+ * @param {Number[]} arr
+ * @param {Number} index1
+ * @param {Number} index2
  */
 function swap(arr, index1, index2) {
-    let temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
+	const temp = arr[index1];
+	arr[index1] = arr[index2];
+	arr[index2] = temp;
 }
 
 console.log('bubbleSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);:', bubbleSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
